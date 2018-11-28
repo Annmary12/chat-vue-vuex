093624882a0f9d1e7e61163e3e95ced0297fcabf
Annmary <annmaryamaka@gmail.com>
2018-11-28 12:18:33 +0100

    - setup vue - setup folder structure

---
+++ src/components/MessageSection/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="message-section">
+    <Message />
+    <Message />
+  </div>
+</template>
+
+<script>
+  import { Component, Vue } from 'vue-property-decorator';
+
+  // components
+  import Message from '../Message/index.vue';
+
+  @Component({
+    components: {
+      Message,
+    }
+  })
+  export default class MessageSection extends Vue {}
+</script>
+
+<style lang="scss">
+  @import './MessageSection';
+</style>
---
+++ src/components/Thread/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="thread">
+    <div class="thread__header">
+      <span class="thread__header--name">Otse</span>
+      <span class="thread__header--time">11.45pm</span>
+    </div>
+    <span class="thread-content">
+      Hi Ann!!!
+    </span>
+  </div>
+</template>
+
+<script>
+  import { Component, Vue } from 'vue-property-decorator';
+
+  export default class Thread extends Vue {}
+</script>
+
+<style lang="scss">
+  @import './Thread';
+</style>
